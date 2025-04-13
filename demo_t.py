421b6048c72234140bb9f4672f98271ee0c72b03
PacoPacorius <c.perdikis@yahoo.gr>
2025-04-13 11:21:25 +0300

    texture map demo added

---
+++ demo_t.py
@@ -0,0 +1,17 @@
+import polygon_fill as pf
+import numpy as np
+import cv2 as cv
+
+vertices = np.array([[1, 4], [6, 2], [8, 11]])
+# colors in opencv's silly BGR format
+vcolors = [[180, 0, 200], [220, 0, 0], [0, 0, 170]]
+depth = np.zeros(20)
+shading = "t"   # "f"lat, "t"exture, "d"ebug (monochrome)
+vertices = np.multiply(vertices, [40])
+
+print('vertices = ', vertices)
+img2 = pf.render_img(vertices, vcolors, depth, shading)
+
+cv.imshow('window', img2)
+cv.moveWindow('window', 0, 0)
+cv.waitKey(0)
