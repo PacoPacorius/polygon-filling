b6f0901b0c81903e9dfb3b449a30f3146fb528f0
PacoPacorius <c.perdikis@yahoo.g>
2025-04-14 14:45:08 +0300

    flat shading convention demo added

--- demo_f.py
+++ demo_f.py
@@ -2,9 +2,30 @@ import polygon_fill as pf
 import numpy as np
 import cv2 as cv
 
-#vertices = np.array([[1, 4], [6, 2], [8, 11],
-                     #[12, 11], [5, 7], [1, 12],
-                     #[5, 5], [5, 10], [10, 10]])
+vertices = np.array([[1, 4], [6, 2], [8, 11],
+                     [12, 11], [5, 7], [1, 12],
+                     [5, 5], [5, 10], [10, 10]])
+
+vertices = np.multiply(vertices, [40])
+L = vertices.shape[0]
+faces = np.array([vertices[0:L:3], vertices[1:L:3], vertices[2:L:3]])
+# colors in opencv's silly BGR format
+vcolors = np.array([[180, 0, 200], [220, 0, 0], [0, 0, 170],
+                    [255, 255, 255], [255, 255, 0,], [255, 255, 255],
+                    [0, 255, 255], [255, 255, 255], [255, 255, 255]])
+depth = np.array([1, 10, 5])
+shading = np.array(["f", "f", "f"])   # "f"lat, "t"exture 
+img = pf.render_img(faces, vertices, vcolors, depth, shading)
+
+print('vertices = ', vertices)
+cv.imshow('depth demo', img)
+cv.moveWindow('depth demo', 0, 0)
+cv.waitKey(0)
+
+
+
+
+
 vertices = np.array([[3, 4], [6, 4], [6, 11],
                      [3, 4], [6, 4], [8, 2],
                      [6, 4], [6, 11], [8, 6]])
@@ -20,6 +41,6 @@ shading = np.array(["f", "f", "f"])   # "f"lat, "t"exture
 img = pf.render_img(faces, vertices, vcolors, depth, shading)
 
 print('vertices = ', vertices)
-cv.imshow('window', img)
-cv.moveWindow('window', 0, 0)
+cv.imshow('conventions demo', img)
+cv.moveWindow('conventions demo', 0, 0)
 cv.waitKey(0)
