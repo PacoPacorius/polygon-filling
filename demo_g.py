4fcfd138a7d0cd6b39d3f9dcd17faa18c646d8a7
PacoPacorius <c.perdikis@yahoo.gr>
2025-04-22 12:44:01 +0300

    rename demo_t.y to demo_g.py

---
+++ demo_g.py
@@ -0,0 +1,19 @@
+import polygon_fill as pf
+import numpy as np
+import cv2 as cv
+
+data = np.load('hw1.npy', None, True).item()
+faces = data['t_pos_idx']
+vertices = data['v_pos2d']
+uvs = data['v_uvs']
+vcolors = data['v_clr']
+depth = data['depth']
+shading = "t"
+print('data = ', data)
+img = pf.render_img(faces, vertices, vcolors, uvs, depth, shading)
+
+cv.imshow('depth demo', img)
+cv.moveWindow('depth demo', 0, 0)
+cv.waitKey(0)
+cv.imwrite('texture_shading.png', img)
+
